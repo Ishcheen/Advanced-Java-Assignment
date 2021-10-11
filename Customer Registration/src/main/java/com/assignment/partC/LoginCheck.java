742368610770c45ee61270208fd1d373931cbacc
Ishcheen <72345411+Ishcheen@users.noreply.github.com>
2021-10-12 02:35:19 +0530

    Permit login if userId and password exists in database.
    1. Added connection to database to retrieve data from userlogin table.
    2. If entered username and password exists in database, then login is successful else user is redirected  to login page.

--- Customer Registration/src/main/java/com/assignment/partC/LoginCheck.java
+++ Customer Registration/src/main/java/com/assignment/partC/LoginCheck.java
@@ -1,6 +1,12 @@
 package com.assignment.partC;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -37,16 +43,42 @@ public class LoginCheck extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PreparedStatement p= null;
+		ResultSet rs=null;
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+		PrintWriter out=response.getWriter();  
 		if(username.isEmpty() || password.isEmpty() ){
 			RequestDispatcher req = request.getRequestDispatcher("Login.html");
 			req.include(request, response);
 		}
-		else{
-			RequestDispatcher req = request.getRequestDispatcher("Profile.jsp");
-			req.forward(request, response);
+		else
+		{
+			try {
+				Class.forName("org.postgresql.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Ishita@99");
+				String sql="select * from party as p join userlogin as u on p.partyid=u.partyid where u.userloginid='"+username+"' and u.password='"+password+"'";
+				
+				p=conn.prepareStatement(sql);
+				rs=p.executeQuery();
+				
+				
+				
+				if(rs.next() && username.equals(rs.getString("userloginid")) && password.equals(rs.getString("password"))) {
+					RequestDispatcher req = request.getRequestDispatcher("Profile.jsp");
+					req.forward(request, response);
+				}
+				else{
+					out.println("UserId or password didn't match.");
+					RequestDispatcher req = request.getRequestDispatcher("/Login.html");
+					req.forward(request, response);
+				}
+			} catch (SQLException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
+		
 	}
 
 }
